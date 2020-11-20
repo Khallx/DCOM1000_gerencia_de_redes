332a4bc51dd60b493709737abdcd2a506ca8d40e
khallx <julio.costella@gmail.com>
2020-11-20 17:44:14 -0300

    add ping sweep and functions to verify change in devices (untested)

--- netscan/netscan.py
+++ netscan/netscan.py
@@ -2,6 +2,10 @@
 
 import os       # cmd line programs
 import time     # for sleep()
+import ipaddress
+import requests
+import subprocess
+import multiprocessing
 from datetime import datetime # for scan date
 
 # netscan scans and logs the current connected network
@@ -12,59 +16,46 @@ from datetime import datetime # for scan date
 # filters a list of string removing whitespace and empty strings
 def filter_strings(string_list):
     sl = string_list
-    sl.remove("")
+    if "" in string_list:
+        sl.remove("")
+
     sl = [s.replace(" ", "")
             .replace("\n", "")
             .replace("\t", "") 
+            .replace("[", "") 
+            .replace("]", "") 
+            .replace("'", "") 
                                 for s in sl]
     return sl
 
 # a single device in the network. Saves info and current time
 class NetworkDevice:
-    def __init__(self, ip, mac, online=True):
+    def __init__(self, ip, mac):
         self.ip = ip
         self.mac = mac 
-        self.vendor = ""
-        self.router = False
         self.online=True 
-        # True if status has been updated since last scan used to determine if a device went offline
-        self.checked_status=True 
+        self.set_vendor()
+        #self.set_router()
         self.first_scan_date = datetime.now()
-        self.status_changed = False # set to true if device went from online to offline by set_status
-
+        
     # two devices are equal if they share the same mac address
     def is_equal(self, compared_device):
         return self.mac == compared_device.mac
         
     # uses cmd line to check if IP has router flag
-    def set_router(self):
-        pass
+    def set_router(self, routers):
+        self.router = False
+        #self.router = self.ip in routers
 
     # uses API to check vendor based on mac addr
     def set_vendor(self):
         pass
-    
-    # checks if status changed
-    def check_status_change(self, online):
-        return online != self.online
+        #self.vendor = requests.get("https://api.macvendors.com/"+self.mac+
+        #" \ -H \"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImp0aSI6IjYwNDI4NTNjLWE2ODEtNGJjMC1hYWEwLTQ4NmViNjg4YzY5MyJ9.eyJpc3MiOiJtYWN2ZW5kb3JzIiwiYXVkIjoibWFjdmVuZG9ycyIsImp0aSI6IjYwNDI4NTNjLWE2ODEtNGJjMC1hYWEwLTQ4NmViNjg4YzY5MyIsImlhdCI6MTYwNTg1NDQyMywiZXhwIjoxOTIwMzUwNDIzLCJzdWIiOiI3OTM0IiwidHlwIjoiYWNjZXNzIn0.0QcT4oFqWzDltiFT2TUfindClv4nCANiJoqtoQgf4xJWz1hBMZTqpLeNcpJWo2qmXaMubLkIWtn59-qVMAc98Q\"").text
 
     # sets device status and report if it is changed
     def set_status(self, online):
-        if self.check_status_change(online):
-            self.report_changed_status()
-            self.online = online
-
-    # set status that indicates if device has been checked and updated its status
-    def set_checked_status(self, checked):
-        self.checked_status = checked
-
-    # chceks if a device is contained in a list 
-    # returns the device from the list or None
-    def get_equal_device_from_list(self, dev_list):
-        for old_dev in dev_list:
-            if(self.is_equal(old_dev)):
-                return old_dev
-        return None
+        self.online = online
 
     # prints current device status as a warning of changed status
     def report_changed_status(self):
@@ -79,18 +70,23 @@ class NetworkDevice:
 
 
 class NetworkScanner:
-    # subnet_addr must include subnet mask in the x.x.x.x/m format
+    # network_addr must include subnet mask in the x.x.x.x/m format
     # when argument is default, ip is used to retrieve the subnet mask
     # scan period defines the time between network scans in (unit of time)
-    def __init__(self, subnet_addr=""):
-        if(subnet_addr==""):
+    def __init__(self, network_addr=""):
+        if(network_addr==""):
             # head -1 used to ensure only a single ip address is retrieved
-            self.subnet_addr = str(os.popen("ip route | grep \"src $MAINIP\""
+           network_addr = str(os.popen("ip route | grep \"src $MAINIP\""
                                     "| awk '{print $1}' | head -1").read())
-        else:
-            self.subnet_addr = subnet_addr
+
+        self.network_addr = ipaddress.ip_network(network_addr)
+        
         self.scanned_devices = [] # list contains history of every device ever scanned
         self.current_scanned_devices = [] # list of current scanned devices
+
+        # network changes from last scan
+        self.new_online_devices_count = 0
+        self.new_offline_devices_count = 0
 
     # does a continuos network scan periodically
     # scan_period = number of seconds between each scan
@@ -110,124 +106,138 @@ class NetworkScanner:
     # updates scanned_devices with new devices and their status
     # updates currrent_scanned_devices with the devices found
     def single_scan(self):
+        pass
         # set the status of every device in history as unchecked
-        # to make sure their status is updated to online of offline according to scan results
-        self.uncheck_scanned_devices_status()
+        # # to make sure their status is updated to online of offline according to scan results
+        # self.uncheck_scanned_devices_status()
 
-        print("[--Scanning--]")
+        # print("[--Scanning--]")
 
-        # TODO: change to ping sweep
-        self.current_scanned_devices = self.scan_host_devices() + self.scan_network_devices() 
+        # # TODO: change to ping sweep
+        # self.current_scanned_devices = self.scan_host_devices() + self.scan_network_devices() 
 
-        # check if the scanned devices are stored in history
-        print("[--Scan Finished--]")
-        print("[--Checking new devices--]")
-        self.update_scanned_list()
-        print("[--Checking offline devices--]")
-        # check if any of the devices went offline
-        self.check_offline_devices()
+        # # check if the scanned devices are stored in history
+        # print("[--Scan Finished--]")
+        # print("[--Checking new devices--]")
+        # self.update_scanned_list()
+        # print("[--Checking offline devices--]")
+        # # check if any of the devices went offline
+        # self.check_offline_devices()
 
 
-    # calls ip addr show and returns matching (ip_list, mac_list)
-    def get_host_ip_mac(self):        
-        # The first line should contain an MAC addr followed by a line containing a IP addr
-        # first grep: filter for link and inet lines that contain mac and ip
-        # second grep: discard lines with loopback addr and ipv6 addr
-        # awk: read only ip and mac addr
-        cmd_out = str(os.popen("ip addr show | grep -e \"link/\" -e \"inet\""
-                            "| grep -v -e \"host lo\" -e \"link/loopback\" -e \"inet6\""
-                            "| awk '{print $2}'").read()) 
+    # pings every possible ip in the network based on
+    # self.max_number_of_devices
+    # returns IPs that could be pinged
+    def ping_sweep(self):
         
-        # split each line into a list
-        cmd_list = cmd_out.splitlines()
-        if "" in cmd_list:
-            cmd_list.remove("")
+        # ping job used for multiprocessor
+        def ping_job(job_q, results_q):
+            DEVNULL = open(os.devnull,'w')
+            while True:
+                ip = job_q.get()
+                if ip is None:break
+                
+                try: 
+                    #subprocess.check_call(['ping','-c1',ip],stdout=DEVNULL)
+                    subprocess.check_call(['ping','-c1',ip])
+                    results_q.put(ip)
+                except: pass
 
-        if len(cmd_list) % 2 != 0:
-            raise ValueError(str(cmd_list))
+        #Create queue for multiprocess
+        jobs,results = multiprocessing.Queue(),multiprocessing.Queue()
 
-        # each odd line contains an ip, even line a mac
-        # returns ip_list, mac_list
-        return cmd_list[1::2], cmd_list[::2]
-
-    # returns list containing NetworkDevices found in host
-    def scan_host_devices(self):
-        ip_list, mac_list = self.get_host_ip_mac()
-
-        host_devices = []
-        for ip, mac in zip(ip_list, mac_list):
-            ip = ip.split("/")[0] # remove subnet mask from addr
-            host_devices.append(NetworkDevice(ip, mac))
-        
-        return host_devices
-
-    # arp scan the network for devices other than the host's
-    # returns ip_list, mac_list with matching values
-    def get_network_ip_mac(self):
-        # arp: return all network addr (excluding host's)
-        # awk: filter to display ip and mac each line
-        # tail: remove arp header
-        cmd_out = str(os.popen("arp | awk '{print $1 \" \" $3}' | tail -n +2").read())
-
-        cmd_list = cmd_out.splitlines()
-        # remove duplicate lines
-        cmd_list = list(set(cmd_list))
-        # split ip and mac 
-        cmd_list = [line.split(" ") for line in cmd_list]
-
-        # return ip_list, mac_list
-        return map(list, zip(*cmd_list))
-
-
-    # returns a list of network devices found by the arp cmd call (does not show host devices)
-    def scan_network_devices(self):
-        ip_list, mac_list = self.get_network_ip_mac()            
-
-        net_devices = [NetworkDevice(ip, mac) for ip,mac in zip(ip_list,mac_list)]
-        return net_devices
-        
-
-    # ------------ network change methods ------------------
-
-    # updates the scanned_devices list based on found devices
-    # as well as updating their status
-    # reports if devices changed status
-    def update_scanned_list(self):
-        # check if device has already been scanned previously.
-        for i, new_dev in enumerate(self.current_scanned_devices):
-            # new devices are added to list
-            equal_dev = new_dev.get_equal_device_from_list(self.scanned_devices)
-            if equal_dev == None:
-                # add new device to scanned devices
-                self.add_new_device(new_dev)
-                # update list with the new information
-                self.current_scanned_devices[i] = new_dev
-            else:
-                equal_dev.set_status(True)
-                # indicate that the status has been updated
-                equal_dev.set_checked_status(True)
-
-                # copy old device information to current scan list
-                self.current_scanned_devices[i] = equal_dev
+        # Create the process to execute ping_sweep
+        pool = [multiprocessing.Process(target=ping_job, args=(jobs,results)) 
+                        for _ in self.network_addr.hosts()]
     
-    def check_offline_devices(self):
-        for dev in self.scanned_devices:
-            # indicates the device was not in the scanned list and must have gone offline
-            if not dev.checked_status and dev.online:                    
-                dev.set_status(False)
-        
-    def uncheck_scanned_devices_status(self):
-        for dev in self.scanned_devices:
-            dev.set_checked_status(False)
+        # Start the process
+        for p in pool: p.start()
+        # Start ping in host
+        for ip in self.network_addr.hosts(): 
+            jobs.put(str(ip))
 
-    def add_new_device(self, dev):
-        # this is done here to avoid too many cmd line and API calls
-        dev.set_router()
-        dev.set_vendor()
-        self.scanned_devices.append(dev)
-        # report new device
-        print("New device found!")
-        dev.print()
+        for p in pool: jobs.put(None)
+
+        # Join all
+        for p in pool: p.join()
+
+        # convert queue to list for easier use later
+        pinged_ips = []
+        while not results.empty():
+            pinged_ips.append(results.get())
+
+        return pinged_ips
+
+
+    def update_scanned_devices(self, pinged_ips):
+        self.new_offline_devices_count = 0
+        self.new_online_devices_count = 0
+
+        # start by checking if any of the devices went offline
+        self.remove_offline_devices(pinged_ips)
+
+        # pinged_ips should now only contain devices
+        # that were not online. We first check 
+        # if they are in scanned_devices 
+        # if not, they are added to both current_scanned_devices
+        # and scanned_devices
+        self.add_devices(pinged_ips)
+
+
+    # rechecks pinged ips and compares with current scanned devices
+    # removes devices from list if they are not found in the pinged ips
+    # removes ips from pinged list if they are found in the current scanned devices
+    def remove_offline_devices(self, pinged_ips):
+        for dev in self.current_scanned_devices:
+            matching_ip = next((ip for ip in pinged_ips if ip == dev.ip), None)
+            if matching_ip == None:
+                # device is now offline
+                self.new_offline_devices_count += 1
+                dev.set_status(False)
+                dev.report_changed_status()
+                self.current_scanned_devices.remove(dev)
+            else:
+                # device is still online, remove ip from list
+                pinged_ips.remove(matching_ip)
+
+    
+    # check self.scanned_devices for ips in pinged_ips
+    # to see if any of the offline devices went online
+    def add_devices(self, pinged_ips):
+        for dev in self.scanned_devices:
+            matching_ip = next((ip for ip in pinged_ips if ip == dev.ip), None)
+            if matching_ip != None:
+                # ip já estava na lsita de devices
+                dev.set_status(True)
+                dev.report_changed_status()
+            else:
+                # ip de dispositivo novo
+                self.add_new_device(matching_ip)
+
+    # creates NetworkDevice object from ip
+    def add_new_device(self, ip):
+        # isso não deveria acontecer na real
+        if self.is_local_ip(ip):
+            pass
+        else:
+            # essa chamada por ip é redundante
+            # mas pode ser usada pra checar se o ip do arp é o mesmo usado como argumento
+            arp_ip = filter_strings(str(os.popen('arp -n '+ip+' | awk \'{print $1}\' | tail -n +2').read()))
+            arp_mac = filter_strings(str(os.popen('arp -n '+ip+' | awk \'{print $3}\' | tail -n +2').read()))
+            dev = NetworkDevice(arp_ip, arp_mac)
+            self.scanned_devices.append(dev)
+            self.current_scanned_devices.append(dev)
+
+    def is_local_ip(self, ip):
+        return ip in self.get_local_ips()
+
+    # this must be called every time 
+    # since one of the machine's interfaces might go offline
+    def get_local_ips(self):
+        local_ips = os.popen('hostname -I').read()
+        local_ips = local_ips.split(" ")
+        local_ips = filter_strings(local_ips)
+        return local_ips
 
     # ---------- Utility methods --------------
     def print_scanned_devices(self):
@@ -243,8 +253,9 @@ class NetworkScanner:
             print("-"*30)
 
 def main():
-    ns = NetworkScanner()
-    ns.periodic_scan(2)
+    network = ipaddress.ip_network("192.168.0.0/24")
+    for x in network.hosts():
+        print(x)
 
 
 if __name__ == "__main__":
