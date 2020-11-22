7827d91f80e69faee9a01137dd4afc775949bb2f
khallx <julio.costella@gmail.com>
2020-11-22 00:50:06 -0300

    add vendor api call, no connection errros fix mac call

--- netscan/netscan.py
+++ netscan/netscan.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+# Authors:
+# Julio Costella Vicenzi
+# Yuri Alves
+# Lucas Pittella
+
 import os       # cmd line programs
 import time     # for sleep()
 import ipaddress
@@ -7,19 +12,23 @@ import requests
 import subprocess
 import multiprocessing
 from datetime import datetime # for scan date
+import argparse
 
 # netscan scans and logs the current connected network
 # and warns of new devices or offline devices
 
-# dependencies: package iproute2 (linux)
+# dependencies: 
+# package iproute2 (linux) for ip 
+# package net-tools (linux) for arp
 
 # filters a list of string removing whitespace and empty strings
 def filter_strings(string_list):
     sl = string_list
-    if "" in string_list:
+    sl = [ filter_string(s) for s in sl ]
+
+    if "" in sl:
         sl.remove("")
 
-    sl = [ filter_string(s) for s in sl]
     return sl
 
 def filter_string(s):
@@ -33,17 +42,13 @@ def filter_string(s):
 
 # a single device in the network. Saves info and current time
 class NetworkDevice:
-    def __init__(self, ip, mac):
+    def __init__(self, ip, mac, UP=True, vendor=""):
         self.ip = ip
         self.mac = mac 
-        self.online=True 
-        self.set_vendor()
+        self.UP=UP 
+        self.vendor = self.get_api_vendor() if vendor == "" else vendor
         #self.set_router()
         self.first_scan_date = datetime.now()
-        
-    # two devices are equal if they share the same mac address
-    def is_equal(self, compared_device):
-        return self.mac == compared_device.mac
         
     # uses cmd line to check if IP has router flag
     def set_router(self, routers):
@@ -51,28 +56,29 @@ class NetworkDevice:
         #self.router = self.ip in routers
 
     # uses API to check vendor based on mac addr
-    def set_vendor(self):
+    def get_api_vendor(self):
         url = "https://api.macvendors.com/"
         api_key = " \ -H \"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImp0aSI6IjYwNDI4NTNjLWE2ODEtNGJjMC1hYWEwLTQ4NmViNjg4YzY5MyJ9.eyJpc3MiOiJtYWN2ZW5kb3JzIiwiYXVkIjoibWFjdmVuZG9ycyIsImp0aSI6IjYwNDI4NTNjLWE2ODEtNGJjMC1hYWEwLTQ4NmViNjg4YzY5MyIsImlhdCI6MTYwNTg1NDQyMywiZXhwIjoxOTIwMzUwNDIzLCJzdWIiOiI3OTM0IiwidHlwIjoiYWNjZXNzIn0.0QcT4oFqWzDltiFT2TUfindClv4nCANiJoqtoQgf4xJWz1hBMZTqpLeNcpJWo2qmXaMubLkIWtn59-qVMAc98Q\""
-        response = requests.get(url+self.mac+api_key)
-        if response.status_code != 200: 
-            self.vendor = "Unknown"
+        try:
+            response = requests.get(url+self.mac+api_key)
+        except Exception as e:
+            print(e)
+        if response.status_code != 200:
+            vendor = "Unknown"
         else:
-            self.vendor = response.content.decode()
+            vendor = response.content.decode()
+        return vendor
 
-    # sets device status and report if it is changed
-    def set_status(self, online):
-        self.online = online
-
-    # prints current device status as a warning of changed status
-    def report_changed_status(self):
-        print("Device changed to ", "ONLINE" if self.online else "OFFLINE" , " !")
+    # prints current device state as a warning of changed state
+    def report_changed_state(self):
+        print("Device changed to ", "UP" if self.UP else "DOWN" , " !")
         self.print()
 
     def print(self):
         print("Device at IP: %s\tMAC: %s" % (self.ip,self.mac))
-        print("Status: ", "Online" if self.online else "Offline")
-        print("Router" if self.router else "Host")
+        print("Vendor: " + self.vendor)
+        print("state: ", "UP" if self.UP else "DOWN")
+        #print("Router" if self.router else "Host")
         print("First scanned at: ", self.first_scan_date.strftime("%d/%m/%Y %H:%M:%S"))
 
 
@@ -86,8 +92,13 @@ class NetworkScanner:
            network_addr = filter_string(str(os.popen("ip route | grep \"src $MAINIP\""
                                     "| awk '{print $1}' | head -1").read()))
 
-        self.network_addr = ipaddress.ip_network(network_addr)
+        try:
+            self.network_addr = ipaddress.ip_network(network_addr)
+        except ValueError:
+            print("Invalid network address. Check your internet connection")
+            exit()
         
+        # TODO: open JSON file and read already scanned devices
         self.scanned_devices = [] # list contains history of every device ever scanned
         self.current_scanned_devices = [] # list of current scanned devices
 
@@ -98,18 +109,23 @@ class NetworkScanner:
     # does a continuos network scan periodically
     # scan_period = number of seconds between each scan
     def periodic_scan(self, scan_period=30):
-        # TODO: open JSON file and read already scanned devices
-        print("Performing continuos network device scan")
+        print("Performing periodic network device scan every "+str(scan_period)+" seconds")
         # continuosly scan 
         while True:
             self.single_scan()
             # TODO: update log with scanned devices
-            self.print_scanned_devices()
+            if self.new_online_devices_count > 0:
+                print(str(self.new_online_devices_count)+" devices are now online!")
+            if self.new_offline_devices_count > 0:
+                print(str(self.new_offline_devices_count)+" devices are now offline!")
+            #self.print_scanned_devices()
+
+            print("[--Scan finished--]")
             time.sleep(scan_period)
 
     # ------------ network scan methods ------------------
     # scans the network for available devices
-    # updates scanned_devices with new devices and their status
+    # updates scanned_devices with new devices and their state
     # updates currrent_scanned_devices with the devices found
     def single_scan(self):
         print("[--Scanning--]")
@@ -120,6 +136,9 @@ class NetworkScanner:
 
         self.update_scanned_devices(addrs_dict)
 
+        if  not self.current_scanned_devices:
+            print("No connected devices found! Check your connection")
+
 
 
     # pings every possible ip in the network based on
@@ -129,13 +148,14 @@ class NetworkScanner:
 
         # ping job used for multiprocessor
         def ping_job(job_q, results_q):
-            DEVNULL = open(os.devnull,'w')
             while True:
                 ip = job_q.get()
                 if ip is None:break
                 
                 try: 
-                    subprocess.check_call(['ping','-c1',ip],stdout=DEVNULL)
+                    subprocess.check_call(['ping','-c1',ip], 
+                                        stdout=subprocess.DEVNULL, 
+                                        stderr=subprocess.DEVNULL)
                     results_q.put(ip)
                 except: pass
 
@@ -149,7 +169,7 @@ class NetworkScanner:
         # Start the process
         for p in pool: p.start()
         # Start ping in host
-        for ip in self.network_addr.hosts(): 
+        for ip in self.network_addr.hosts():
             jobs.put(str(ip))
 
         for p in pool: jobs.put(None)
@@ -180,20 +200,18 @@ class NetworkScanner:
                                         "| grep "+grep_ip+" | awk \'{print $NF}\'").read()))
         # get interface mac
         mac = filter_string(str(os.popen("ip link show "+ip_interface+""
-                                        "| awk \'{print $2}\' | tail -n 2").read()))
+                                        "| awk \'{print $2}\' | tail -n +2").read()))
         return mac
 
     # returns a dictionary with mac as keys and ip as items
+    # TODO: verify macs could actually be retrieved.
     def get_macs(self, pinged_ips):
         addrs_dict = {}
         # local ips mac addr cannot be resolved via arp
         local_ips = self.get_local_ips()
         
         for ip in pinged_ips:
-            if ip in local_ips:
-                mac = self.get_local_mac(ip)
-            else: 
-                mac = self.get_mac_by_arp(ip)
+            mac = self.get_local_mac(ip) if ip in local_ips else self.get_mac_by_arp(ip)
             # add ip mac pair
             addrs_dict[mac] = ip
         
@@ -206,11 +224,9 @@ class NetworkScanner:
         # start by checking if any of the devices went offline
         self.remove_offline_devices(addr_dict)
 
-        # pinged_ips should now only contain devices
-        # that were not online. We first check 
-        # if they are in scanned_devices 
-        # if not, they are added to both current_scanned_devices
-        # and scanned_devices
+        # addr_dic should now only contain devices
+        # that were not online. We first check if they are in scanned_devices 
+        # if not, they are added to both current_scanned_devices and scanned_devices
         self.add_devices(addr_dict)
 
     # Checks addr_dict and compares with current scanned devices
@@ -230,8 +246,8 @@ class NetworkScanner:
             else:
                 # device is now offline
                 self.new_offline_devices_count += 1
-                dev.set_status(False)
-                dev.report_changed_status()
+                dev.UP = False
+                dev.report_changed_state()
                 self.current_scanned_devices.remove(dev)
 
     # check self.scanned_devices for ips in pinged_ips
@@ -239,8 +255,7 @@ class NetworkScanner:
     def add_devices(self, addr_dict):
         for dev in self.scanned_devices:
             if dev.mac in addr_dict.keys():
-                # mac already exists in past scanned devices,
-                # set it as online
+                # mac already exists in past scanned devices
 
                 # TODO: refatorar código repetido
                 dict_ip = addr_dict[dev.mac]
@@ -248,15 +263,17 @@ class NetworkScanner:
                     print("IP on device changed from "+dev.ip+" to "+dict_ip+" !")
                     dev.ip = dict_ip
                
-                dev.set_status(True)
-                dev.report_changed_status()
+                dev.UP = True
+                dev.report_changed_state()
+                # add device to current scanned devices
+                self.current_scanned_devices.append(dev)
                 del addr_dict[dev.mac]
 
         # the remaning devices must be new
         for mac, ip in addr_dict.items():
             self.add_new_device(ip, mac)
 
-    # creates NetworkDevice object from ip
+    # creates NetworkDevice object from ip and append to lists
     def add_new_device(self, ip, mac):
         self.new_online_devices_count += 1
         dev = NetworkDevice(ip, mac)
@@ -286,11 +303,10 @@ class NetworkScanner:
             dev.print()
             print("-"*30)
 
+
 def main():
     ns = NetworkScanner()
-    print(ns.ping_sweep())
-    mac = ns.get_mac_by_arp("192.168.0.1")
-    print(mac)
+    ns.periodic_scan(10)
 
 if __name__ == "__main__":
     main()
